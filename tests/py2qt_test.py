3e6b7f80dfcbaed13f3f361e9e78fb5e9d4ec362
Nicolas Garcia <garciatunonnicolas@gmail.com>
2023-05-31 10:21:57 +0200

    dev

---
+++ tests/py2qt_test.py
@@ -0,0 +1,35 @@
+import sys
+import os
+import codecs
+from PySide2 import QtWidgets, QtCore
+from PySide2.QtWidgets import QApplication, QMainWindow
+
+
+os.environ['QT_MAC_WANTS_LAYER'] = '1'
+
+app = QApplication(sys.argv)
+
+
+
+class Test(QtWidgets.QWidget):
+    def __init__(self):
+        
+        super(Test, self).__init__(None, QtCore.Qt.Tool)
+        self.label = QtWidgets.QLabel()
+        self.button = QtWidgets.QPushButton('press me')
+        self.button.released.connect(self.call_button_pressed)
+        layout = QtWidgets.QVBoxLayout(self)
+        layout.addWidget(self.label)
+        layout.addWidget(self.button)
+
+    def call_button_pressed(self):
+        self.label.setText(codecs.encode('ABBO !', 'rot_13'))
+
+
+
+
+window = Test()
+window.show()
+
+# Start the event loop.
+app.exec_()
