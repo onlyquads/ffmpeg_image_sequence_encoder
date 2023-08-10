f6626853159ef1d7c33e34957242bd6c3063932e
Nicolas Garcia <garciatunonnicolas@gmail.com>
2023-08-11 00:03:01 +0200

    code cleanup

--- ffmpeg_image_sequence_encoder/functions.py
+++ ffmpeg_image_sequence_encoder/functions.py
@@ -4,6 +4,7 @@ import subprocess as sp
 
 tool_name = 'FFmpeg Image Sequence Encoder'
 
+
 def get_ffmpeg_path():
 
     # MAKE SURE FFMPEG PATH IS SETUP CORRECTLY FOR YOUR PLATFORM HERE:
@@ -26,15 +27,15 @@ def open_dir(path):
     fixed_path = os.path.normpath(path)
     print('Trying to open directory path: ' + fixed_path)
     if os.path.isdir(fixed_path):
-        if platform.system()=='Windows':
-        
+        if platform.system() == 'Windows':
+
             cmd = ['explorer', fixed_path]
 
-        elif platform.system()=='Darwin':
+        elif platform.system() == 'Darwin':
 
             cmd = ['open', '%s' % fixed_path]
 
-        elif platform.system()=='Linux':
+        elif platform.system() == 'Linux':
             cmd = ['xdg-open', '%s' % fixed_path]
         sp.call(cmd)
     else:
@@ -44,7 +45,7 @@ def open_dir(path):
 def get_in_out_files(file_path):
     dir_path = os.path.dirname(file_path)
     full_file_name = os.path.basename(file_path)
-    base_file_name, file_extension  = os.path.splitext(full_file_name)
+    base_file_name, file_extension = os.path.splitext(full_file_name)
     file_name = base_file_name
     clean_file_name = file_name[0:-4]
     input_file_name = dir_path + '/' + clean_file_name
@@ -54,50 +55,48 @@ def get_in_out_files(file_path):
     return input_file_name, output_file_name, file_extension
 
 
-
 def encode_with_ffmpeg(input_file, output_file, file_extension, fps_value):
 
-    input_file = os.path.normpath(input_file) +'%04d'+file_extension
+    input_file = os.path.normpath(input_file) + '%04d' + file_extension
     output_file = os.path.normpath(output_file) + '.mov'
     ffmpeg_path = os.path.normpath(get_ffmpeg_path())
 
-    ### THE CRF FLAG IS FOR QUALITY CONSTANT, FROM  0 TO 63
-    if file_extension == '.exr':    
+    # THE CRF FLAG IS FOR QUALITY CONSTANT, FROM  0 TO 63
+    if file_extension == '.exr':
         cmd = [
             ffmpeg_path,
             '-y',
             '-gamma', '2.2',
             '-i', input_file,
             '-r', fps_value,
-            '-vcodec','libx264',
-            '-pix_fmt','yuv420p',
+            '-vcodec', 'libx264',
+            '-pix_fmt', 'yuv420p',
             '-crf', '18',
             output_file
         ]
 
-    else: 
+    else:
         cmd = [
             ffmpeg_path,
             '-y',
             '-i', input_file,
             '-r', fps_value,
-            '-vcodec','libx264',
-            '-pix_fmt','yuv420p',
+            '-vcodec', 'libx264',
+            '-pix_fmt', 'yuv420p',
             '-crf', '18',
             output_file
         ]
-    
+
     process = sp.Popen(cmd, stdout=sp.PIPE, stderr=sp.PIPE)
-    
+
     print ('Encoding Started')
-    
+
     # Wait for the process to finish and capture its output
     stdout, stderr = process.communicate()
 
     # Check the return code to determine if the process completed successfully
     if process.returncode == 0:
         print('Process completed successfully!')
-        
+
     else:
         print('Process failed with return code:', process.returncode)
-
--- ffmpeg_image_sequence_encoder/main.py
+++ ffmpeg_image_sequence_encoder/main.py
@@ -1,11 +1,12 @@
-
-import os 
+import os
 import sys
 from PySide2 import QtWidgets
-from PySide2.QtWidgets import QMainWindow, QWidget, QVBoxLayout,QLabel, QHBoxLayout, QLineEdit, QPushButton, QFileDialog, QFrame, QSpinBox
+from PySide2.QtWidgets import (
+    QMainWindow, QWidget, QVBoxLayout, QLabel, QHBoxLayout, QLineEdit,
+    QPushButton, QFileDialog, QFrame, QSpinBox)
 import functions
 
-### FOR MAC OS WE NEED THIS LINE FOR PYTHON 2.7
+# FOR MAC OS WE NEED THIS LINE FOR PYTHON 2.7
 os.environ['QT_MAC_WANTS_LAYER'] = '1'
 
 
@@ -14,15 +15,15 @@ class Encoding_Window(QMainWindow):
 
         super(Encoding_Window, self).__init__()
         self.setWindowTitle(functions.tool_name)
-        
+
         # Create a central widget and layout
         central_widget = QWidget()
         layout = QVBoxLayout(central_widget)
-        
+
         # Create a text field for the file path
         self.filepath_field = QLineEdit()
         layout.addWidget(self.filepath_field)
-        
+
         # Create a browse button
         browse_button = QPushButton('Browse')
         browse_button.clicked.connect(self.browse_files)
@@ -59,13 +60,14 @@ class Encoding_Window(QMainWindow):
         layout.addWidget(open_dir_button)
 
         self.setCentralWidget(central_widget)
-        
+
     def browse_files(self):
         # Open the file browser and filter for supported image files
         file_dialog = QFileDialog()
         file_dialog.setFileMode(QFileDialog.ExistingFile)
-        file_dialog.setNameFilter('EXR Sequence (*.exr *.jpg *.jpeg *.tiff *.png)')
-        
+        file_dialog.setNameFilter(
+            'EXR Sequence (*.exr *.jpg *.jpeg *.tiff *.png)')
+
         if file_dialog.exec_():
             selected_files = file_dialog.selectedFiles()
             self.filepath_field.setText(selected_files[0])
@@ -76,17 +78,19 @@ class Encoding_Window(QMainWindow):
         # Get the file path
         file_path = self.filepath_field.text()
         # Get the input name formated with %04d and the output file
-        input_file_name, output_file_name, file_extension = functions.get_in_out_files(file_path)
+        input_name, output_name, ext = functions.get_in_out_files(file_path)
         # Start the encoding process
-        functions.encode_with_ffmpeg(input_file_name, output_file_name, file_extension, fps_value)
-    
+        functions.encode_with_ffmpeg(
+            input_name, output_name, ext, fps_value)
+
     def open_dir(self):
         file_path = self.filepath_field.text()
         path = os.path.dirname(file_path)
         functions.open_dir(path)
 
+
 if __name__ == '__main__':
-    
+
     if not QtWidgets.QApplication.instance():
         app = QtWidgets.QApplication(sys.argv)
     else:
@@ -95,7 +99,7 @@ if __name__ == '__main__':
     window = Encoding_Window()
     window.show()
     app.exec_()
-    
+
 
 def show():
     window = Encoding_Window()
